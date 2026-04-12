337423b7277c948e9a380102914ddf1a0025f450
szymon <szymon.pt4sznik@gmail.com>
2026-04-12 15:27:53 +0200

    added repositories for user, data injeciton to html

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,13 +1,17 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repositories/UsersRepository.php';
 
 class DashboardController extends AppController {
     public function index() {
         // TODO
         // pobieranie danych z bazy
         // wstawianie danych na widok
+        $usersRepository = new UsersRepository();
+        $users = $usersRepository->getUsers();
+
         $title = 'HGW';
-        return $this->render("dashboard", );
+        return $this->render("dashboard", ["title" => $title, "users" => $users]);
     }
 }
